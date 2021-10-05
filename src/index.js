f3ec5aacbcbfe54e52437d4ce2cf061b1e41ee5e
Angel Bryan <angelbryan98@outlook.com>
2021-10-05 00:21:54 -0500

    Repositorio creado

---
+++ src/index.js
@@ -0,0 +1,54 @@
+import fslightbox from 'fslightbox';
+
+fsLightboxInstances['first-lightbox'].open(0);
+fsLightboxInstances['second-lightbox'].props.onOpen = () => console.log('Lightbox open!');
+
+/*
+
+import {suma, saludos} from "./operaciones.js"
+import Swal from 'sweetalert2'
+
+document.write (`<h1>${suma(1,2)}</h1>`)
+
+console.log(suma(1, 2));
+console.log(saludos);
+
+
+Swal.fire({
+    title: 'Ángel Bryan Gallegos Guzmán',
+    text: 'Ángel Bryan Gallegos Guzmán',
+    imageUrl: 'https://unsplash.it/400/200',
+    imageWidth: 400,
+    imageHeight: 200,
+    imageAlt: 'Custom image',
+  })
+
+*/
+  
+/*
+
+Swal.fire({
+    title: 'Error!',
+    text: 'Do you want to continue',
+    icon: 'error',
+    confirmButtonText: 'Cool'
+  })
+
+* /
+
+/*
+
+import _ from 'lodash';
+
+function component() {
+    const element = document.createElement('div');
+  
+    // Lodash, currently included via a script, is required for this line to work
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  
+    return element;
+  }
+  
+  document.body.appendChild(component());
+
+  */
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import {suma, saludos} from "./operaciones.js"
+
+console.log(suma(num1, num2));
+console.log(saludos);
+
